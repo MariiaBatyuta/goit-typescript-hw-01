0baf39ec8f3d64a47de51954a5f0af7d3b57c1e7
MariiaBatyuta <mashabatyuta.work@gmail.com>
2024-04-09 16:10:23 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
+import concatenation from "./concatenation";
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+if (input && button) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+        input.value = '';
+    });
+}
 function add(num1, num2) {
     return console.log(num1 + num2);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function add(num1: number, num2: number):void {
+import concatenation from "./concatenation";
+
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+if (input && button) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+        input.value = '';
+    })
+}
+
+function add(num1: number, num2: number): void {
     return console.log(num1 + num2);
 }
 
-add(2,3)
+add(2, 3);
